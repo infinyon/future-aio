7ea4ce33901e14e62ab58c991d5bf3ff9898a0d5
Sehyo Chang <sehyo@fluvio.io>
2020-03-21 17:30:19 -0700

    update the test with lagged case add channel to broadcoast

--- src/future-aio/src/sync/broadcast.rs
+++ src/future-aio/src/sync/broadcast.rs
@@ -9,21 +9,52 @@ pub use tokio::sync::broadcast::RecvError;
 
 
 
+
+#[derive(Debug)]
+pub struct Channel<T> {
+    receiver: Receiver<T>,
+    sender: Sender<T>
+}
+
+impl <T>Channel<T> {
+
+    pub fn new(capacity: usize) -> Self {
+
+        let (sender,receiver) = channel(capacity);
+        Self {
+            receiver,
+            sender
+        }
+    }
+
+    /// create new clone of sender
+    pub fn receiver(&self) -> Receiver<T> {
+        self.sender.subscribe()
+    }
+
+
+    pub fn sender(&self) -> Sender<T> {
+        self.sender.clone()
+    }
+
+   
+}
+
+
+
 #[cfg(test)]
 mod test {
     use std::time::Duration;
 
     use log::debug;
-    use futures::stream::StreamExt;
-    use futures::select;
+
 
     use crate::timer::sleep;
     use crate::test_async;
     use crate::task::spawn;
     
+    use super::Channel;
     use super::Receiver;
-    use super::Sender;
-    use super::channel;
     use super::RecvError;
 
 
@@ -52,8 +83,8 @@ mod test {
                             assert_eq!(sum,10);
                             return;
                         },
-                        RecvError::Lagged(_) => {
-                            debug!("lagging");
+                        RecvError::Lagged(lag) => {
+                            debug!("conn: {}, lagging: {}",count,lag);
                         }
                     }
                     
@@ -70,26 +101,31 @@ mod test {
     #[test_async]
     async fn test_conn() -> Result<(), ()> {
         
-        let (tx, receiver): (Sender<u16>,Receiver<u16>) = channel(100);
+        let channel = Channel::new(2);
 
         // create multiple receiver
 
+        // need to create dummy receiver
         for i in 0..2 {
-            spawn(test_receive(tx.subscribe(), i));
+            spawn(test_receive(channel.receiver(), i));
         }
+
+       // spawn(test_receive(receiver,2));
 
         sleep(Duration::from_millis(100)).await;
 
         //let sender2 = sender.clone();
 
+        let sender = channel.sender();
+
         for i in 0..5 {
-            tx.send(i as u16).expect("should be sent");
+            sender.send(i as u16).expect("should be sent");
+            sleep(Duration::from_millis(1)).await;
         }
 
         sleep(Duration::from_millis(100)).await;
         debug!("waiting 5 seconds");
-        drop(tx);
-        drop(receiver);
+        drop(channel);
 
         sleep(Duration::from_millis(100)).await;
         debug!("finished test");
