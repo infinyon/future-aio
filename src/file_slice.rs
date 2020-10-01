4384df3406a9c36f612dd8f0636f23315ee3cf27
Sehyo Chang <sehyo@fluvio.io>
2020-09-30 21:54:49 -0700

    update dependencies (#26)
    * update dependencies
    update tls test
    
    * fmt

--- src/file_slice.rs
+++ src/file_slice.rs
@@ -1,26 +1,18 @@
-
 use std::os::unix::io::AsRawFd;
 use std::os::unix::io::RawFd;
 
-
-
 /// Slice of the file
 /// This works only on raw fd
-#[derive(Default,Debug,Clone)]
+#[derive(Default, Debug, Clone)]
 pub struct AsyncFileSlice {
     fd: RawFd,
     position: u64,
-    len: u64
+    len: u64,
 }
 
 impl AsyncFileSlice {
-
-    pub fn new(fd: RawFd,position: u64,len: u64) -> Self {
-        Self {
-            fd,
-            position,
-            len
-        }
+    pub fn new(fd: RawFd, position: u64, len: u64) -> Self {
+        Self { fd, position, len }
     }
 
     pub fn position(&self) -> u64 {
@@ -34,15 +26,10 @@ impl AsyncFileSlice {
     pub fn fd(&self) -> RawFd {
         self.fd
     }
-
-
 }
-
 
 impl AsRawFd for AsyncFileSlice {
-
     fn as_raw_fd(&self) -> RawFd {
         self.fd
-    }   
+    }
 }
-
