2cbdc110d8d65f41b206b0b7c716e7ad5e647b5e
Sehyo Chang <sehyo@fluvio.io>
2020-11-15 23:10:32 -0800

    fix semver breakage on sleep (#39)
    * fix semver breaking on sleep by impl custom future for sleep
    add after fn
    bump up version
    * cargo fmt

--- src/timer.rs
+++ src/timer.rs
@@ -2,10 +2,35 @@ pub use inner::*;
 
 mod inner {
 
-    use std::time::{Duration, Instant};
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
+    use std::time::Duration;
 
     use async_io::Timer;
     use futures_lite::future::Future;
+
+    use pin_project::pin_project;
+
+    /// same as `after` but return () to make it compatible as previous
+    pub fn sleep(duration: Duration) -> Sleeper {
+        Sleeper(after(duration))
+    }
+
+    #[pin_project]
+    pub struct Sleeper(#[pin] Timer);
+
+    impl Future for Sleeper {
+        type Output = ();
+
+        fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            let this = self.project();
+            if let Poll::Ready(_) = this.0.poll(cx) {
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        }
+    }
 
     /// wait for until `duration` has elapsed.
     ///
@@ -14,14 +39,14 @@ mod inner {
     /// # Examples
     ///
     /// ```
-    /// use fluvio_future::timer::sleep;
+    /// use fluvio_future::timer::after;
     /// use std::time::{Duration, Instant};
     ///
     /// fluvio_future::task::run(async {
-    ///     sleep(Duration::from_secs(1)).await;
+    ///     after(Duration::from_secs(1)).await;
     /// });
     /// ```
-    pub fn sleep(duration: Duration) -> impl Future<Output = Instant> {
+    pub fn after(duration: Duration) -> Timer {
         Timer::after(duration)
     }
 }
@@ -60,6 +85,7 @@ mod test {
 
         debug!("total time elaspsed: {:#?}", elapsed);
         assert!(elapsed < Duration::from_millis(30));
+        assert!(elapsed > Duration::from_millis(10));
         assert_eq!(sleep_count, 10);
 
         Ok(())
