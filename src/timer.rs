44ad3166b9d9ddb0e44acc7b9cc7114d83c28c86
Sehyo Chang <sehyo@fluvio.io>
2020-11-15 21:17:14 -0800

    make sleep fn unpinnable by removing extra await (#38)
    * make sleep fn unpinnable by turn removing extra await and turn into
    futures
    bump up version
    
    * apply cargo fmt
    
    * increase time out

--- src/timer.rs
+++ src/timer.rs
@@ -2,11 +2,66 @@ pub use inner::*;
 
 mod inner {
 
-    use std::time::Duration;
+    use std::time::{Duration, Instant};
 
     use async_io::Timer;
+    use futures_lite::future::Future;
 
-    pub async fn sleep(duration: Duration) {
-        Timer::after(duration).await;
+    /// wait for until `duration` has elapsed.
+    ///
+    /// this effectively give back control to async execution engine until duration is finished
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use fluvio_future::timer::sleep;
+    /// use std::time::{Duration, Instant};
+    ///
+    /// fluvio_future::task::run(async {
+    ///     sleep(Duration::from_secs(1)).await;
+    /// });
+    /// ```
+    pub fn sleep(duration: Duration) -> impl Future<Output = Instant> {
+        Timer::after(duration)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use std::time::Duration;
+    use std::time::Instant;
+
+    use log::debug;
+    use tokio::select;
+
+    use fluvio_future::test_async;
+    use fluvio_future::timer::sleep;
+
+    /// test timer loop
+    #[test_async]
+    async fn test_sleep() -> Result<(), ()> {
+        let mut sleep_count: u16 = 0;
+        let time_now = Instant::now();
+
+        let mut sleep_ft = sleep(Duration::from_millis(10));
+
+        for _ in 0u16..10u16 {
+            select! {
+                _ = &mut sleep_ft => {
+                    // fire everytime but won't make cause more delay than initial 10 ms
+                    sleep_count += 1;
+                    debug!("timer fired");
+                }
+            }
+        }
+
+        let elapsed = time_now.elapsed();
+
+        debug!("total time elaspsed: {:#?}", elapsed);
+        assert!(elapsed < Duration::from_millis(30));
+        assert_eq!(sleep_count, 10);
+
+        Ok(())
     }
 }
