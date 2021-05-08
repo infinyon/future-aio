b3a611e9d42b924d19bfa9ef1ddc04a936e87dea
Sehyo <sehyo@fluvio.io>
2021-05-08 04:13:31 +0000

    add option to add attribute on test add `test` attribute which doesnot require result

--- async-test-derive/src/lib.rs
+++ async-test-derive/src/lib.rs
@@ -8,16 +8,22 @@ use proc_macro2::Span;
 
 
 #[proc_macro_attribute]
-pub fn test_async(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn test_async(args: TokenStream, item: TokenStream) -> TokenStream {
 
+    use syn::AttributeArgs;
+
+    let attribute_args = syn::parse_macro_input!(args as AttributeArgs);
     let input = syn::parse_macro_input!(item as ItemFn);
     let name = &input.sig.ident;
     let sync_name = format!("{}_sync",name);
     let out_fn_iden = Ident::new(&sync_name, Span::call_site());
 
+    let test_attributes = generate::generate_test_attributes(&attribute_args);
+
     let expression = quote! {
 
         #[test]
+        #test_attributes
         fn #out_fn_iden()  {
 
             ::fluvio_future::subscriber::init_logger();
@@ -40,4 +46,66 @@ pub fn test_async(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     expression.into()
 
+}
+
+
+#[proc_macro_attribute]
+pub fn test(args: TokenStream, item: TokenStream) -> TokenStream {
+
+    use syn::AttributeArgs;
+
+    let attribute_args = syn::parse_macro_input!(args as AttributeArgs);
+    let input = syn::parse_macro_input!(item as ItemFn);
+    let name = &input.sig.ident;
+    let sync_name = format!("{}_sync",name);
+    let out_fn_iden = Ident::new(&sync_name, Span::call_site());
+
+    let test_attributes = generate::generate_test_attributes(&attribute_args);
+
+    let expression = quote! {
+
+        #[test]
+        #test_attributes
+        fn #out_fn_iden()  {
+
+            ::fluvio_future::subscriber::init_logger();
+
+            #input
+
+            let ft = async {
+                #name().await;
+            };
+
+            
+            ::fluvio_future::task::run_block_on(ft);
+
+        }
+    };
+
+    expression.into()
+
+}
+
+
+
+mod generate {
+
+    use proc_macro2::TokenStream;
+    use syn::NestedMeta;
+    use quote::quote;
+
+    pub fn generate_test_attributes(attributes: &Vec<NestedMeta>) -> TokenStream {
+        
+        let args = attributes.iter().map(|meta| {
+            quote! {
+                #[#meta]
+            }
+        });
+
+        quote! {
+
+            #(#args)*
+
+        }
+    }
 }
---
+++ async-test-derive/ui-tests/pass_ignore.rs
@@ -0,0 +1,12 @@
+use fluvio_future::test_async;
+
+
+#[test_async(ignore)]
+async fn test_simple() {
+    assert_eq!(2,2);
+}
+
+
+
+fn main() {
+}
