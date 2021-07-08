dfe63b6d923d1c16ce81c768ac44abbcc68e7e1d
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-07-07 18:01:01 -0700

    cargo fmt

--- src/openssl/connector.rs
+++ src/openssl/connector.rs
@@ -16,8 +16,8 @@ use log::debug;
 use openssl::ssl;
 
 use crate::net::{
-    BoxReadConnection, BoxWriteConnection, DomainConnector, SplitConnection, TcpDomainConnector, ConnectionFd,
-    TcpStream,
+    BoxReadConnection, BoxWriteConnection, ConnectionFd, DomainConnector, SplitConnection,
+    TcpDomainConnector, TcpStream,
 };
 
 use super::async_to_sync_wrapper::AsyncToSyncWrapper;
