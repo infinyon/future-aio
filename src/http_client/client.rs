34e1d88c4a34205e9d960aa56194b713b1ad48a4
Matheus Consoli <matheus.consoli7@gmail.com>
2023-10-19 14:39:45 -0300

    Remove http-client ClientBuilder

--- src/http_client/client.rs
+++ src/http_client/client.rs
@@ -2,6 +2,7 @@ use std::{str::FromStr, sync::Arc};
 
 use async_rustls::rustls::{OwnedTrustAnchor, RootCertStore};
 use hyper::Uri;
+use once_cell::sync::Lazy;
 
 use super::{
     async_std_compat::{self, CompatConnector},
@@ -15,35 +16,23 @@ pub struct Client {
     pub(crate) hyper: HyperClient,
 }
 
-impl Client {
-    pub fn new() -> Self {
-        ClientBuilder::default().build()
-    }
+static ROOT_CERT_STORE: Lazy<RootCertStore> = Lazy::new(|| {
+    let mut cert = RootCertStore::empty();
+    cert.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+        OwnedTrustAnchor::from_subject_spki_name_constraints(
+            ta.subject,
+            ta.spki,
+            ta.name_constraints,
+        )
+    }));
+    cert
+});
 
-    pub fn get(&self, uri: impl AsRef<str>) -> RequestBuilder {
-        let req = http::request::Builder::new().uri(Uri::from_str(uri.as_ref()).unwrap());
-        RequestBuilder::new(self.clone(), req)
-    }
-}
-
-#[derive(Default)]
-#[must_use]
-pub struct ClientBuilder;
-
-impl ClientBuilder {
-    pub fn build(self) -> Client {
-        let mut cert = RootCertStore::empty();
-        cert.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
-            OwnedTrustAnchor::from_subject_spki_name_constraints(
-                ta.subject,
-                ta.spki,
-                ta.name_constraints,
-            )
-        }));
-
+impl Default for Client {
+    fn default() -> Self {
         let tls = async_rustls::rustls::ClientConfig::builder()
             .with_safe_defaults()
-            .with_root_certificates(cert)
+            .with_root_certificates(ROOT_CERT_STORE.to_owned())
             .with_no_client_auth();
 
         let https = async_std_compat::CompatConnector::new(tls);
@@ -55,5 +44,16 @@ impl ClientBuilder {
         Client {
             hyper: Arc::new(client),
         }
+    }
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn get(&self, uri: impl AsRef<str>) -> RequestBuilder {
+        let req = http::request::Builder::new().uri(Uri::from_str(uri.as_ref()).unwrap());
+        RequestBuilder::new(self.clone(), req)
     }
 }
